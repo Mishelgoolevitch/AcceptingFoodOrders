0c3ee4cc475d435e11f481b0ddb15010b116a28b
mixail <v1tus5555@yandex.by>
2026-06-20 15:48:30 +0300

    Добавляем контроллер MenuController и представление Index в папке Menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using AcceptingFoodOrders.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcceptingFoodOrders.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? categoryId)
+        {
+            
+            var categories = _context.Categories.ToList();
+            ViewBag.Categories = categories;
+
+            var foodItems = _context.FoodItems
+                .Include(f => f.Category)
+                .Where(f => f.IsAvailable && (categoryId == null || f.CategoryId == categoryId))
+                .ToList();
+
+            return View(foodItems);
+        }
+    }
+}
