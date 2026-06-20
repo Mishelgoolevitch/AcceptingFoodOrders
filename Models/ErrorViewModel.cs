db7fdd708dec2f4a6e1163c78d188d786cf6232e
mixail <v1tus5555@yandex.by>
2026-06-20 07:10:06 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcceptingFoodOrders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
