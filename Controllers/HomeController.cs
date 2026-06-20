0e3e3d69e56ab8028126fdd68545d7ea5aa74ac5
mixail <v1tus5555@yandex.by>
2026-06-20 12:49:25 +0300

    Добавлено представление Index.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,21 @@ namespace AcceptingFoodOrders.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext _context)
         {
-            _logger = logger;
+            context = _context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
+            var footitems = context.FoodItems
+                .Where(f => f.IsAvailable)
+                .Take(6)
+                .ToList();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(footitems);
         }
     }
 }
