d82f15a20d6efbc72d64bb4e8fc96b7cdef24317
mixail <v1tus5555@yandex.by>
2026-06-21 18:40:30 +0300

    Cоздаем класс LoginViewModel.cs и представление Login.cshtml

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcceptingFoodOrders.Models;
+using AcceptingFoodOrders.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AcceptingFoodOrders.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    Username = model.Username,
+                    Email = model.Email,
+                    Password = model.Password, 
+                    FullName = model.FullName,
+                    Address = model.Address,
+                    Phone = model.Phone
+                };
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return RedirectToAction("Menu");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
