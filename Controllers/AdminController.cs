6bc834b3014a14384bf6f5ca62e4bfa69c221856
mixail <v1tus5555@yandex.by>
2026-07-22 10:00:28 +0300

    Добавляем контроллер AdminController.cs и метод действия Dashboard() и создаем представление Dashboard.cshtml

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using AcceptingFoodOrders.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcceptingFoodOrders.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private bool IsAdmin()
+        {
+            return HttpContext.Session.GetString("IsAdmin") == "True";
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (!IsAdmin()) return RedirectToAction("Index", "Home");
+
+            ViewBag.TotalOrders = _context.Orders.Count();
+            ViewBag.TotalUsers = _context.Users.Count();
+            ViewBag.TotalItems = _context.FoodItems.Count();
+            return View();
+        }
+    }
+}
